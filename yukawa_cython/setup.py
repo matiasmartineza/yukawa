7ce761bc2453a4a788440cd630af2dfe65d23223
Matías Martínez <matiasmartinezalvarado@gmail.com>
2017-11-22 12:03:37 -0300

    Serialización de códigos que deberías ser seriales.

--- yukawa_cython/setup.py
+++ yukawa_cython/setup.py
@@ -5,11 +5,11 @@ from Cython.Build import cythonize
 import os
 import numpy as np
 
-ext = Extension("P2Pwrap", sources=["P2Pwrap.pyx"], language = "c", include_dirs = [np.get_include()], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp'],)
+ext = Extension("P2Pwrap", sources=["P2Pwrap.pyx"], language = "c", include_dirs = [np.get_include()], extra_compile_args=[''], extra_link_args=['-'],)
 setup(name="P2Pwrap", ext_modules = cythonize([ext]))
 
-ext = Extension("calculateMultipoles", sources=["calculateMultipoles.pyx"], language = "c", include_dirs = [np.get_include()], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp'],)
+ext = Extension("calculateMultipoles", sources=["calculateMultipoles.pyx"], language = "c", include_dirs = [np.get_include()], extra_compile_args=[''], extra_link_args=[''],)
 setup(name="calculateMultipoles", ext_modules = cythonize([ext]))
 
-ext = Extension("M2Pwrap", sources=["M2Pwrap.pyx"], language = "c", include_dirs = [np.get_include()], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp'],)
+ext = Extension("M2Pwrap", sources=["M2Pwrap.pyx"], language = "c", include_dirs = [np.get_include()], extra_compile_args=[''], extra_link_args=[''],)
 setup(name="M2Pwrap", ext_modules = cythonize([ext]))
